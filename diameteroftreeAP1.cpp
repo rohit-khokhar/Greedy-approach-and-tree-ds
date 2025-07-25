22876500ec535355fb921a30d8c5d801b9754f64
Rohit Khokhar <victorkhokhar3@gmail.com>
2025-07-25 11:58:44 +0530

    Update diameteroftreeAP1.cpp
    "Optimized diameter calculation in DSA implementation: Reduced time complexity from O(n^2) to O(n) by recursively calculating height and diameter using pairing in diam2 function"

--- diameteroftreeAP1.cpp
+++ diameteroftreeAP1.cpp
@@ -60,12 +60,32 @@ int Dia1(Node* root){
 }
 
 
+pair<int, int>diam2(Node* root){ 
+    if(root == NULL){
+        return make_pair(0,0);
+    }
+
+    //  (diameter,height)
+    pair<int, int> leftInfo = diam2(root->left);  
+    pair<int, int> RighttInfo = diam2(root->right);
+
+    int currDiam = leftInfo.second + RighttInfo.second + 1;
+    int finalDia = max(currDiam, max(leftInfo.first,RighttInfo.first));
+    int finalHt = max(leftInfo.second,RighttInfo.second) + 1;
+
+    return make_pair(finalDia,finalHt);
+
+}
+
+
 int main()
 {
     vector<int> nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
    Node* root = buildTree(nodes);
 
     cout << "Diameter = "<< Dia1(root) << endl;
+    cout << "Diameter = "<< diam2(root).first<< endl;
+
 
 
     return 0;
